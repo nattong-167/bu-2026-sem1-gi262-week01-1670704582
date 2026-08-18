14c760e324c36088b510fac1e52e20a5bfc3d23b
nattong-167 <nat.tong@bumail.net>
2026-08-18 13:05:14 +0700

    Update-(The door, Knock)

--- Assets/Scripts/Workshop.cs
+++ Assets/Scripts/Workshop.cs
@@ -6,7 +6,7 @@ public class Workshop : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // As01_SyntaxIf();
+        As01_SyntaxIf();
         // As02_StringComparisonExample();
         // As03_NumberComparisonExample();
         // As04_AndOrOperatorExample();
@@ -18,7 +18,10 @@ public class Workshop : MonoBehaviour
     public bool isSixOClock;
     public void As01_SyntaxIf()
     {
-        
+        if (isSixOClock) {
+            Debug.Log("The door Open");
+        }
+        Debug.Log("Knock Knock!");
     }
 
     public string password;
